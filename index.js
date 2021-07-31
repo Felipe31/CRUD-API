2e9e26b291ce549e11815f34afd138fd6ba4130b
Felipe Soares <felipe31soares@gmail.com>
2021-07-31 01:35:47 -0300

    Add CORS configuration

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('./model/connection')
 const express = require('express')
 const routes  = require('./routes')
 const app     = express()
+const cors    = require('cors');
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000)
+app.listen(3030)
 
 module.exports = app
